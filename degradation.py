05bc09dc3a61ccb0a601375c08683aec2a1d471e
Tatyana140589 <tatyana.kalimullina@abbyy.com>
2020-01-27 03:29:18 +0300

    writing degradation into file was fixed

--- degradation.py
+++ degradation.py
@@ -18,14 +18,18 @@ for i in range(2, ws.max_row+1):
         fps_dldt_cpu8[ws.cell(row=i, column=1).value] = int(ws.cell(row=i, column=3).value)
 
 for t in topology :
-    if (fps_dldt_cpu8[t] == -7) or (fps_dldt_cpu8[t] == -3):
+    if not(t in fps_dldt_cpu8):
+        degradation[t] = ''
+    elif (fps_dldt_cpu8[t] == -7) or (fps_dldt_cpu8[t] == -3) :
         degradation[t] = "Error"
     else:
         degradation[t] = (fps_dldt_cpu8[t] - fps_dldt_cpu32[t]) / fps_dldt_cpu32[t]
 
+for i in range(2, ws.max_row+1):
+    if ws.cell(row=i, column=2).value == 'dldt_cpu8':
+        ws.cell(row=i, column=4, value=degradation[ws.cell(row=i, column=1).value])
 
-
-
+wb.save('degradation.xlsx')
 
 print(degradation)
 print(ws.max_column)
