3c8353d3c658f671bd9ce7f821bea267ddc00869
Tatyana140589 <tatyana.kalimullina@abbyy.com>
2020-01-27 04:05:41 +0300

    cells color is fixed

--- degradation.py
+++ degradation.py
@@ -1,5 +1,8 @@
 from openpyxl import Workbook
 from openpyxl import load_workbook
+from openpyxl.styles import PatternFill
+import datetime
+
 
 wb = Workbook()
 wb = load_workbook('./Results.xlsx')
@@ -13,9 +16,9 @@ for i in range(2, ws.max_row+1):
     if not(ws.cell(row=i, column=1).value in topology):
         topology.append(ws.cell(row=i, column=1).value)
     if ws.cell(row=i, column=2).value == "dldt_cpu32":
-        fps_dldt_cpu32[ws.cell(row=i, column=1).value] = int(ws.cell(row=i, column=3).value)
+        fps_dldt_cpu32[ws.cell(row=i, column=1).value] = float(ws.cell(row=i, column=3).value)
     else:
-        fps_dldt_cpu8[ws.cell(row=i, column=1).value] = int(ws.cell(row=i, column=3).value)
+        fps_dldt_cpu8[ws.cell(row=i, column=1).value] = float(ws.cell(row=i, column=3).value)
 
 for t in topology :
     if not(t in fps_dldt_cpu8):
@@ -25,13 +28,19 @@ for t in topology :
     else:
         degradation[t] = (fps_dldt_cpu8[t] - fps_dldt_cpu32[t]) / fps_dldt_cpu32[t]
 
+redFill = PatternFill(start_color='FF2400',
+                   end_color='FF2400',
+                   fill_type='solid')
+
 for i in range(2, ws.max_row+1):
     if ws.cell(row=i, column=2).value == 'dldt_cpu8':
         ws.cell(row=i, column=4, value=degradation[ws.cell(row=i, column=1).value])
+        if (ws.cell(row=i, column=4).value !='' and ws.cell(row=i, column=4).value !='Error') and float(ws.cell(row=i, column=4).value) < -5:
+            ws.cell(row=i, column=4).fill = redFill
 
-wb.save('degradation.xlsx')
+now = datetime.datetime.now().strftime('%Y-%m-%d_%H-%M-%S')
+wb.save('degradation_'+str(now)+'.xlsx')
 
-print(degradation)
-print(ws.max_column)
+
 
 
